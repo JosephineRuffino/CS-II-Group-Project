daa9819ae3bcfc62b9cd2458cb7d7502f5c2a3ef
Beida04 <89615225+Beida04@users.noreply.github.com>
2021-11-12 11:50:33 -0600

    Update Product.java

--- Part 2/Main Program/Product.java
+++ Part 2/Main Program/Product.java
@@ -1,12 +1,12 @@
 
 public class Product {
-	
+	//create variables
 	private int productID;
 	private String productName;
 	private int productQuantity;
 	private double productPrice;
 	
-	public Product (int productID, String productName, int productQuantity, double productPrice)
+	public Product (int productID, String productName, int productQuantity, double productPrice) //construct the product class 
 	{
 		this.productID=productID;
 		this.productName=productName;
@@ -14,24 +14,24 @@ public class Product {
 		this.productPrice=productPrice;
 	}
 	
-	public void modify (String productName, int productQuantity, double productPrice)
+	public void modify (String productName, int productQuantity, double productPrice) //construct a product after new inputs are entered 
 	{
 		this.productName=productName;
 		this.productQuantity=productQuantity;
 		this.productPrice=productPrice;
 	}
 	
-	public String getProductName()
+	public String getProductName() //returns a product name 
 	{
 		return productName;
 	}
 	
-	public double getProductPrice()
+	public double getProductPrice() // returns a product price 
 	{
 		return productPrice;
 	}
 	
-	public String toString()
+	public String toString() //constructs and prints the sentence for a product 
 	{
 		return productID + " " + productName + " $" + productPrice;
 	}
