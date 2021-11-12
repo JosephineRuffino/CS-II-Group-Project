6cafa95c6a99a5fb3179dc0e92e991db97144bd3
Beida04 <89615225+Beida04@users.noreply.github.com>
2021-11-12 11:58:57 -0600

    Update Service.java

--- Part 2/Main Program/Service.java
+++ Part 2/Main Program/Service.java
@@ -1,33 +1,34 @@
 	
 public class Service {
+	         //create variables
 		private int serviceID;
 		private double serviceCharge;
 		private String serviceType;
 		
-		public Service (int serviceID, String serviceType, double serviceCharge)
+		public Service (int serviceID, String serviceType, double serviceCharge) //construct the Service class 
 		{
 			this.serviceID = serviceID;
 			this.serviceType = serviceType;
 			this.serviceCharge = serviceCharge;
 		}
 		
-		public void modify (String serviceType, double serviceCharge)
+		public void modify (String serviceType, double serviceCharge) //construct a service after new inputs are entered 
 		{
 			this.serviceType = serviceType;
 			this.serviceCharge = serviceCharge;
 		}
 		
-		public String getServiceType()
+		public String getServiceType() //resturns services type 
 		{
 			return serviceType;
 		}
 		
-		public double getServiceCharge()
+		public double getServiceCharge() //returns service charge 
 		{
 			return serviceCharge;
 		}
 		
-		public String toString()
+		public String toString() //construct and print sentence for services 
 		{
 			return serviceID + " " + serviceType + " $" + serviceCharge;
 		}
