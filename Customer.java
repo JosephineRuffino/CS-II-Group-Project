1de82e3f5b32c367927cbef5ceb26f544dc6e116
NoeESR <89822053+NoeESR@users.noreply.github.com>
2021-11-11 18:55:03 -0700

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,31 @@
+public class Customer{
+	
+    private int customerId,streetNumber;
+	private long phoneNumber;
+    private String fName, lName, streetName;
+  
+		public Customer(int customerId, String fName, String lName, int streetNumber, String streetName, long phoneNumber){
+		
+			this.customerId =customerId;
+			this.fName =fName;
+			this.lName= lName;
+			this.streetName = streetName;
+			this.streetNumber= streetNumber;
+			this.phoneNumber =phoneNumber;
+		}
+		
+		public void modify (String fName, String lName, int streetNumber, String streetName, long phoneNumber)
+		{
+			this.fName =fName;
+			this.lName= lName;
+			this.streetNumber= streetNumber;
+			this.streetName = streetName;
+			this.phoneNumber =phoneNumber;
+		}
+
+		public String toString()
+		{
+			return "Customer Id: "+ customerId + "\nName: "+ fName+ " "+ lName+ "\nAdress: "+ streetNumber+ " "+ streetName + "\nPhone Number:  " + phoneNumber;
+		}
+	}
+	
